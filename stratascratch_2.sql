d359ee5e1095060110614827535b0757d5df3af1
Evgeniy <99033931+EvgeniyChygrynec@users.noreply.github.com>
2022-11-21 10:41:29 +0200

    Add files via upload

---
+++ stratascratch_2.sql
@@ -0,0 +1,11 @@
+--Given a table of purchases by date, calculate the month-over-month percentage
+--change in revenue. The output should include the year-month date (YYYY-MM) and
+--percentage change, rounded to the 2nd decimal point, and sorted from the beginning
+--of the year to the end of the year. The percentage change column will be populated
+--from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue)
+--/ last month's revenue)*100.
+
+select DATE_FORMAT(created_at, '%Y-%m') as ym,
+round((sum(value) - lag(sum(value)) over()) / (lag(sum(value)) over()) *100,2) as revenue_diff_pct
+from sf_transactions
+group by ym
